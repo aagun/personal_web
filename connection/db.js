6dc759047257b59d0a37000e2fca342e8429ad38
Agun <agunfahminurhakiki@gmail.com>
2021-12-31 03:05:21 +0700

    change config

--- connection/db.js
+++ connection/db.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 
 const dbPool = new Pool({
-  database: 'personal_web_b29',
-  port: 5432,
-  user: 'postgres',
-  password: 'root',
+  // database: 'personal_web_b29',
+  // port: 5432,
+  // user: 'postgres',
+  // password: 'root',
+  connectionString:
+    'postgres://rgmsybiqbbwcui:333cec9d3964a901bfce415394b496e72fa87fc3de9ebdf9f80d175ec1eb1deb@ec2-52-70-205-234.compute-1.amazonaws.com:5432/dban8a6lk16hvq',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = dbPool;
